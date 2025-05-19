99ad8227013f2f7121eab3946840234311b1e491
ElBadry <mohammedbadry2003@gmail.com>
2025-05-19 09:27:11 +0100

    Init commit

---
+++ src/main/java/com/elbadry/mohammed/ElBadryMohammedExamJeeApplication.java
@@ -0,0 +1,13 @@
+package com.elbadry.mohammed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElBadryMohammedExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElBadryMohammedExamJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elbadry/mohammed/ElBadryMohammedExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbadry.mohammed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElBadryMohammedExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
