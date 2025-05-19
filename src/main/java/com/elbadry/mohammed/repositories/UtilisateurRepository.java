cb49082766dbe295c4a8148c957dd7a0c59abbd7
ElBadry <mohammedbadry2003@gmail.com>
2025-05-19 10:42:06 +0100

    Add RemboursementRepository and UtilisateurRepository interfaces

---
+++ src/main/java/com/elbadry/mohammed/repositories/UtilisateurRepository.java
@@ -0,0 +1,13 @@
+package com.elbadry.mohammed.repositories;
+
+import com.elbadry.mohammed.entities.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    Optional<Utilisateur> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
